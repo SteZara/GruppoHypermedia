072e60759f1fc115547e59a51cc73baec2461c2a
luigimintrone <luigi.mintrone@polimi.it>
2025-05-13 22:57:01 +0200

    added index.vue with related icons

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,96 @@
-<script setup lang="ts">
-// import Cat_card from "~/components/cat_card.vue";
-
-// const featured = [
-//   {
-//     name: 'Fluffy',
-//     img: '/cat1.png'
-//   },
-//   {
-//     name: 'Whiskers',
-//     img: '/cat2.png'
-//   },
-//   {
-//     name: 'Ginger',
-//     img: '/cat3.png'
-//   }
-// ]
-</script>
-
 <template>
-  <breadcrumb/>
-  <h1>Home page</h1>
-  <!-- Hero Section -->
-  <!--<section class="hero">
-    <h2>Discover the Purrfect Companion!</h2>
-  </section>-->
-
-  <!-- Main Content Section -->
-  <!--<section class="main-content">
-    <h2>Featured Cats</h2>
-    <div class="cat-gallery">
-      <Cat_card v-for="cat of featured" :cat="cat"/>
+  <div class="index-container">
+    <div class="main-image">
+      <img src="/center/yoga_sunset.jpg" alt="Yoga meditazione" />
     </div>
-    <NuxtLink class="ml" to="/cats">See More Cats →</NuxtLink>
-  </section>-->
+
+    <div class="icon-link top-left">
+      <NuxtLink to="/about">
+        <img src="/icons/about.png" alt="About Icon" />
+        <span>about us</span>
+      </NuxtLink>
+    </div>
+
+    <div class="icon-link top-right">
+      <NuxtLink to="/careers">
+        <img src="/icons/career.png" alt="Careers Icon" />
+        <span>careers</span>
+      </NuxtLink>
+    </div>
+
+    <div class="icon-link bottom-left">
+      <NuxtLink to="/teachers">
+        <img src="/icons/teachers.png" alt="Teachers Icon" />
+        <span>instructors</span>
+      </NuxtLink>
+    </div>
+
+    <div class="icon-link bottom-right">
+      <NuxtLink to="/activities">
+        <img src="/icons/activity.png" alt="Activity Icon" />
+        <span>activity</span>
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 
+<script setup lang="ts">
+// nessuna logica per ora
+</script>
+
 <style scoped>
-.hero {
-  background: url('/shelter.png') no-repeat center center/cover;
-  height: 400px;
+.index-container {
+  position: relative;
+  height: calc(100vh - 160px);
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.main-image img {
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  object-fit: cover;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+}
+
+.icon-link {
+  position: absolute;
   text-align: center;
-  color: #fff;
+  width: 120px;
 }
 
-.hero h2 {
-  font-size: 2.5em;
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+.icon-link img {
+  width: 40px;
+  height: 40px;
+  margin-bottom: 5px;
 }
 
-.cat-gallery {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: stretch;
-  gap: 20px;
-  flex-wrap: wrap;
-}
-
-.cat-gallery > * {
-  flex-basis: 0;
-}
-
-.ml{
+.icon-link span {
   display: block;
-  text-align: end;
-  padding: 30px;
+  font-size: 14px;
+  color: #333;
+  font-weight: 600;
 }
 
+.top-left {
+  top: 15%;
+  left: 22%;
+}
+
+.top-right {
+  top: 15%;
+  right: 22%;
+}
+
+.bottom-left {
+  bottom: 15%;
+  left: 22%;
+}
+
+.bottom-right {
+  bottom: 15%;
+  right: 22%;
+}
 </style>
